ab3a7a94d41915bf906ea0343636f18718b18eb3
toma-kazuki <kaz6032kaz@g.ecc.u-tokyo.ac.jp>
2024-05-28 12:40:42 +0900

    create setup.m

---
+++ user/+toka/+introduction/+systems/setup.m
@@ -0,0 +1,18 @@
+classdef setup < simulation.infrastructure
+    properties
+
+    end
+
+    methods
+        function this = setup(ptConfigParams, tSolverParams) 
+            
+        end
+
+        function configureMonitors(this)
+
+        end
+
+        function plot(this)
+        end
+    end
+end
