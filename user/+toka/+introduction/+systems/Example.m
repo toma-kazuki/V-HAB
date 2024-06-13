ad750cb39584ca749ccb381c7b2eeb89cfd5d0c9
toma-kazuki <kaz6032kaz@g.ecc.u-tokyo.ac.jp>
2024-06-13 22:12:38 +0900

    add solid

--- user/+toka/+introduction/+systems/Example.m
+++ user/+toka/+introduction/+systems/Example.m
@@ -19,7 +19,12 @@ classdef Example < vsys
             matter.store(this, 'Condensate_Storage', 1); % キャビンのストアが追加される
             matter.store(this, 'Waste_Storage', 10); % キャビンのストアが追加される
             matter.store(this, 'Vacuum', 1e6); % キャビンのストアが追加される
-        	matter.phases.gas(this.toStores.Cabin, 'CabinAir', struct('N2', 1), 1, 293.15); % キャビンのストアに1キログラムの窒素の気体フェーズが追加される
+
+            matter.phases.solid(this.toStores.Waste_Storage, 'Solid_Waste', struct('C42H69O13N5',0.1), 293); %宇宙飛行士が出す廃棄物の一部(固形廃棄物コンパートメント内の化学物質は、人間の糞便の組成の近似値としてよく使用されるもの)
+
+            components.matter.FoodStore(this, 'Food',  100, struct('Food', 100)); %ISSで使用される宇宙飛行士の標準的な食事
+
+        	matter.phases.gas(this.toStores.Cabin, 'CabinAir', struct('N2', 1), 1, 293.15); % キャビンのストアに1キログラム の窒素の気体フェーズが追加される
         end
        
         function createSolverStructure(this)
